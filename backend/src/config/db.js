4b3e2b409a34c420080b3dc189a042902da12a2c
HaiK22TT <nchai.k22tt@kontum.udn.vn>
2026-05-10 15:45:08 +0700

    fix backend not submodule

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,9 @@
-const mysql = require("mysql2/promise")
+const mysql = require("mysql2")
 
-const db = mysql.createPool({
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT) || 3306
+  port: process.env.DB_PORT
 })
-
-module.exports = db
